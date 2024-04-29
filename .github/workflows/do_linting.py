c3f85dc60abc1d93b37685dbe4d8e44e8bae60d5
krauhen <167354464+krauhen@users.noreply.github.com>
2024-04-29 14:22:14 +0200

    Update do_linting.py

--- .github/workflows/do_linting.py
+++ .github/workflows/do_linting.py
@@ -1,4 +1,15 @@
-import sys
+import pylint
 
-for arg in sys.argv:
-    print(arg)
+with open(".pylint_whitelist.txt", "r") as f:
+    lines = f.readlines()
+
+for line in lines:
+    print(line)
+    print()
+    (pylint_stdout, pylint_stderr) = lint.py_run(line, return_std=True)
+    print("pylint_stdout")
+    print(pylint_stdout)
+    print()
+    print("pylint_stderr")
+    print(pylint_stderr)
+    print()
